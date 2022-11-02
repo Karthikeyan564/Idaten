fc507a36b994d4649e2c0a0ebe4eb97522098232
Preethaa J <preethaaj.io@gmail.com>
2022-11-02 13:16:03 +0530

    half pixel calc for half pixel calc

--- src/FME/half_ip.v
+++ src/FME/half_ip.v
@@ -21,38 +21,72 @@
 
 /* int_pix_ind - address of the centre integer pixel
  half - interpolated value*/
+ 
+module counter(cnt,clk,rst,en);
+input clk,rst,en;
+output [2:0]cnt;
 
-module half_ip(input clk, input [7:0] int_pix_ind, input [3:0] cand, output reg [47:0] pixels);
+reg [2:0]cnt;
+wire [2:0]next_cnt;
+
+assign next_cnt = cnt + 1'b1;  //Just increment by 1
+
+always @ (posedge clk or negedge rst)   
+begin
+ if(!rst & en)
+ begin
+ cnt <= 3'b0;
+ end
+ else if (en)
+ begin
+ cnt <= next_cnt;
+ end
+end
+
+endmodule 
+ 
+
+module half_ip(input clk, rst, input [7:0] int_pix_ind, input [3:0] cand, output reg [47:0] pixels);
 
 reg [7:0] lut [255:0];
-wire [47:0] pixels_int;
+reg [47:0] pixels_int;
 reg [7:0] half_int [11:0];
-
-wire [7:0] temp_address;
+reg [7:0] temp_address;
+wire [2:0] i;
+wire en;
 
 initial begin
     $readmemh("mbvalues_ref.mem" ,lut); 
 end
 
 six_tf dut1 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[0]));    
-six_tf dut2 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[1]));
-six_tf dut3 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[2]));
-six_tf dut4 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[3]));
-six_tf dut5 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[4]));
-six_tf dut6 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[5]));
-six_tf dut7 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[6]));    
-six_tf dut8 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[7]));
-six_tf dut9 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[8]));
-six_tf dut10 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[9]));
-six_tf dut11 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[10]));
-six_tf dut12 (.clk(clk), .a(pixels_int[7:0]), .b(pixels_int[15:8]), .c(pixels_int[23:16]), .d(pixels_int[31:24]), .e(pixels_int[39:32]), .f(pixels_int[47:40]), .half(half_int[11]));
+counter dut2 (.cnt(i), .clk(clk), .rst(rst), .en(en));
 
-always @ int_pix_ind
-
-
+always @ i
 begin
 
-half_int[0] <= 
+case(i)
+ 3'b000 : temp_address <= int_pix_ind;
+ 3'b001 : temp_address <= int_pix_ind -48;
+ 3'b010 : temp_address <= int_pix_ind -32;
+ 3'b011 : temp_address <= int_pix_ind -16;
+ 3'b100 : temp_address <= int_pix_ind +16;
+ 3'b101 : temp_address <= int_pix_ind +32;
+ default : en <= 1'b0;
+endcase
+
+
+end
+
+always @ temp_address
+begin
+
+pixels_int [7:0] <= lut[temp_address - 3];
+pixels_int [15:8] <= lut[temp_address - 2];
+pixels_int [23:16] <= lut[temp_address - 1];
+pixels_int [31:24] <= lut[temp_address];
+pixels_int [39:32] <= lut[temp_address+1];
+pixels_int [47:40] <= lut[temp_address+2];
 
 end
 
@@ -87,7 +121,6 @@ case (cand)
                pixels [39:32] <= lut [int_pix_ind +1];
                pixels [47:40] <= lut [int_pix_ind +2];
                end  
-4'b0000: begin 
 
 endcase
 endmodule
