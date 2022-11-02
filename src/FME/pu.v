32163926e2a9d37d69e0a0a8e6cf6110303389d2
Preethaa J <preethaaj.io@gmail.com>
2022-11-02 15:07:52 +0530

    1d transform module

--- src/FME/pu.v
+++ src/FME/pu.v
@@ -19,8 +19,26 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module oneDtrans(input [7:0] rc1, rc2, rc3, rc4, output reg [7:0] op1, op2, op3, op4);
 
-module pu();
+wire [7:0] i1, i2, i3, i4;
+
+assign i1 = rc1 + rc4;
+assign i2 = rc2 + rc3;
+assign i3 = rc2 - rc3;
+assign i4 = rc1 - rc4;
+
+always @ rc1
+begin
+op1 <= i1 + i2;
+op2 <= i3 + i4;
+op3 <= i1 - i2;
+op4 <= i4 - i3;
+end
+
+endmodule
+
+module pu(input [15:0][7:0] ref_pix, cur_pix, output [7:0] distortion);
 
 
 
