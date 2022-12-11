8e90f9b739ed9f2238e1c406cedd0cd33b8d5d59
Preethaa J <preethaaj.io@gmail.com>
2022-12-11 21:03:32 +0530

    bug fixes

--- src/FME/satd_gen.v
+++ src/FME/satd_gen.v
@@ -20,7 +20,22 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module satd_gen(
+module satd_gen(input clk, rst1, input [8:0][7:0] half_quat , input [15:0][7:0] cur_pix, output [3:0] best);
 
-    );
+wire [3:0] state [8:0];
+wire on [8:0];
+reg [8:0][15:0] distort;
+wire com_en;
+
+genvar i;
+generate
+    for (i=0; i<9; i++) begin
+    main_fsm dut2 (.clk(clk), .rst(rst1),.ref_pix(half_quat[i]), .cur_pix(cur_pix), .state(state[i]), .on(on[i]), .distort(distort[i]));
+    end
+endgenerate
+
+assign com_en = on[0] ? 1'b0 : 1'b1;
+
+comparator dutx (.clk(clk), .en(com_en), .distort(distort), .best(best)); 
+
 endmodule
