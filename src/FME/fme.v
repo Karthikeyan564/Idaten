3aaedc6e44943093ec3349906decd934236b7ef7
Preethaa J <preethaaj.io@gmail.com>
2022-12-18 02:01:05 +0530

    main module initial commit

--- src/FME/fme.v
+++ src/FME/fme.v
@@ -20,7 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module fme(
+module fme(input clk, rst, input[7:0] pix_pos, output [7:0] quat_val);
 
-    );
+reg [8:0][7:0] half, quat ;
+reg rst1, rst2;
+wire [3:0] best;
+wire quat_en;
+wire [8:0][7:0] int_pix;
+
+half_ip dutq (.clk(clk), .rst(rst), .int_ind_pix(pix_pos), .half(half));
+satd_gen dutw (.clk(clk), .rst1(rst1), .half_quat(half), .cur_pix(cur_pix), .best(best));
+quat_ip dute (.en(quat_en), .int_pix(int_pix), .half_cand(half), .best(best), .quat(quat));
+satd_gen dutr (.clk(clk), .rst1(rst2), .half_quat(quat), .cur_pix(cur_pix), .best(best));
+
+//state machine to feed signal
+
 endmodule
