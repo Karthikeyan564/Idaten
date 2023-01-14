30cc215645c503783260e3a213efaf46005815c9
RaamyPi <sriramharshalee@gmail.com>
2023-01-14 20:39:27 +0530

    Values dumped to env now

--- src/EncoderIntra/encoder_intra.sv
+++ src/EncoderIntra/encoder_intra.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 module encoder_intra #(
-    parameter LENGTH = 32,
-    parameter WIDTH = 32)(
+    parameter LENGTH = 16,
+    parameter WIDTH = 16)(
     input clk,
     input reset,
     input enable,
